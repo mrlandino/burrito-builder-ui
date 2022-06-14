c2969a83a62adf2f4e11f6bd32fa9e708dbda239
Ross Landino <mrlandino@gmail.com>
2022-06-14 12:14:47 -0600

    Complete testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,16 +1,17 @@
 describe('Orders Page', () => {
   beforeEach(() => {
-      cy.visit('http://localhost:3000')
-      cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {fixture: 'orders'}).as('orders')
-      cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
-            statusCode: 201,
-            body: {
-                id: 4,
-                name: 'Ross',
-                ingredients: []
-            }
-      })
+    cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {fixture: 'orders'}).as('orders')
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 201,
+      body: {
+        id: 4,
+        name: 'Jim',
+        ingredients: ['bean, lettuce']
+      }
+    })
+    cy.visit('http://localhost:3000')
   })
+
   it('should confirm that true equals true', () => {
     expect(true).to.equal(true)
   })
@@ -19,20 +20,19 @@ describe('Orders Page', () => {
     cy.contains('h1', 'Burrito Builder')
   })
 
-
   it('should have a form to add a burrito order to the orders', () => {
     cy.get('input[name="name"]')
-      .type('Ross')
-      .should("have.value", "Ross")
+      .type('Jim')
+      .should("have.value", "Jim")
     cy.get('form').contains('button', 'beans').click()
     cy.get('form').contains('p', 'beans' )
     cy.get('form').contains('button', 'lettuce').click()
     cy.get('form').contains('p', 'beans, lettuce' )
     cy.get('form').contains('button', 'Submit Order').click()
+
+    cy.get('section')
+    .contains('.order', 'Jim')
   })
 
-  it('should have orders displayed on the page on page load', () => {
-    cy.get('section')
-      .contains('.order', 'Ross')
-  })
+  
 })
